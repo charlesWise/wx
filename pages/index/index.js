a90fba86e77da4db3a3b31af635c2183ba21cef6
lianqin <chenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.comchenrunsheng@lianqin360.com>
2017-01-13 18:18:09 +0800

    微信小程序初始化

---
+++ pages/index/index.js
@@ -0,0 +1,36 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      'https://img.tourongjia.com/data/uploads/img/2017/01/06/586f4a5f7f5fa.jpg',
+      'https://img.tourongjia.com/data/uploads/img/2017/01/03/586b7416e4bb4.jpg',
+      'https://img.tourongjia.com/data/uploads/img/2016/08/05/57a466227fb3e.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
+    dotsClass: ['active','','']
+  },
+  // swiper组件改变事件
+  swiperChange: function(event) {
+    var dotsClass = ['','','','',''];
+    dotsClass[event.detail.current] = 'active';
+    this.setData({
+      dotsClass
+    });
+  },
+  onLoad() {
+    console.log('app', app)
+  },
+  //小程序显示的时候触发
+  onShow() {
+    console.log('onShow')
+  },
+  //小程序隐藏的时候触发
+  onHide() {
+    console.log('onHide')
+  }
+})
